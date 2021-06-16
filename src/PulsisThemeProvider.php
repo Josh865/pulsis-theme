6b7292710c92f54bc9631e127a21204bb0a69b8e
Josh <white167@purdue.edu>
2021-06-16 15:29:03 -0400

    wip

--- src/PulsisThemeProvider.php
+++ src/PulsisThemeProvider.php
@@ -15,16 +15,30 @@ class PulsisThemeProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'pulsis-theme');
 
-        $this->publishes([
-            __DIR__ . '/../resources/views/components' => resource_path(
-                'views/theme/components',
-                'components'
-            ),
-            __DIR__ . '/../resources/views/layout' => resource_path(
-                'views',
-                'layout'
-            ),
-            __DIR__ . '/../public' => public_path('theme', 'assets'),
-        ]);
+        $this->publishes(
+            [
+                __DIR__ . '/../resources/views/components' => resource_path(
+                    'views/theme/components'
+                ),
+            ],
+            'components'
+        );
+
+        $this->publishes(
+            [
+                __DIR__ .
+                '/../resources/views/layout.blade.php' => resource_path(
+                    'views'
+                ),
+            ],
+            'layout'
+        );
+
+        $this->publishes(
+            [
+                __DIR__ . '/../public' => public_path('theme'),
+            ],
+            'assets'
+        );
     }
 }
